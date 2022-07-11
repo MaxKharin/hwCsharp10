e88857b092726a6eb0897ba3a1d19ac3285d7861
MaximKharin <maximkharin@gmail.com>
2022-07-11 18:37:01 +0200

    finished homework

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу,
+// которая выведет все чётные натуральные числа в промежутке
+// от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int numberM = Prompt("Enter M number, please: ");
+int numberN = Prompt("Enter N number, please: ");
+Console.WriteLine();
+Console.WriteLine($"All even natural numbers in range from {numberM} to {numberN}: ");
+PrintNaturalEvenNumbers(numberM, numberN);
+
+void PrintNaturalEvenNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        return;
+    }
+    if (numberN % 2 == 1)
+    {
+        numberN--;
+    }
+
+    PrintNaturalEvenNumbers(numberM, numberN - 2);
+    Console.WriteLine($"{numberN}");
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке
+// от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int numberM = Prompt("Enter M number, please: ");
+int numberN = Prompt("Enter N number, please: ");
+Console.WriteLine();
+int sum = PrintSumOfNaturalElements(numberM, numberN);
+Console.WriteLine($"The sum of all natural elements in range from {numberM} to {numberN} is {sum}");
+
+int PrintSumOfNaturalElements(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        return 0;
+    }
+
+    return PrintSumOfNaturalElements(numberM, numberN - 1) + numberN;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
